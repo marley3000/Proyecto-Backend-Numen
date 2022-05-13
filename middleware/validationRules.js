3f2ed6e7cf2ee0984b0ce97b66a07a66395397d5
Martin Facchinello <martinfac@hotmail.com>
2022-05-13 11:58:50 -0300

    Aumento de caracteres max en opinion

--- middleware/validationRules.js
+++ middleware/validationRules.js
@@ -7,7 +7,7 @@ exports.rules = [
     check("number_travelers").not().isEmpty().withMessage("Indicar la cantidad de personas que participaron del viaje"),
     check("duration_days").not().isEmpty().withMessage("Debe indicar la duración del viaje"),
     check("valoration").not().isEmpty().withMessage("Indique la valoración del destino").isFloat({min:1, max:10}).withMessage("La valoración debe estar entre 1 y 10, siendo 1 el puntaje más bajo y 10 el más alto"),
-    check("opinion").not().isEmpty().withMessage("Debe colocar una opinión del destino").isLength({max:150, min:5}).withMessage("La opinión debe contener entre 6 y 149 caracteres")
+    check("opinion").not().isEmpty().withMessage("Debe colocar una opinión del destino").isLength({max:300, min:5}).withMessage("La opinión debe contener entre 6 y 299 caracteres")
 ];
 
 exports.checks = (req, res, next) => {
