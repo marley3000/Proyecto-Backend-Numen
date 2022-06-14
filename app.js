6ac564f153b30d7796987bb046c7d176cdf24c9e
Martin Facchinello <martinfac@hotmail.com>
2022-06-14 14:26:09 -0300

    Modif 'dev' por 'start'

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const {dbConnection} = require('./db/db')
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger('start'));
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
