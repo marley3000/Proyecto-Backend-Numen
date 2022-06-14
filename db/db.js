cbc1d85f324e80d15bf29db5b588794156e556b6
Martin Facchinello <martinfac@hotmail.com>
2022-06-14 16:47:55 -0300

    cambio db

--- db/db.js
+++ db/db.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {  
-        await mongoose.connect( process.env.MONGO_CNN, () => {
-           
-            console.log('Base de datos conectada');
+        await mongoose.connect( process.env.MONGO_CNN,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         } )
+        console.log('Base de datos conectada');
     } catch {
         console.log('Error a la hora de iniciar la base de datos');
     }
